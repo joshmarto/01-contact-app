4ec38805ec6f80c1701c5302d49d8683445276f3
Josue Martinez <jmartinezolivares1@gmail.com>
2022-10-21 07:18:31 -0600

    Route groups

--- routes/web.php
+++ routes/web.php
@@ -25,25 +25,27 @@ Route::get('/', function () {
     return $html;
 })->name('');
 
-Route::get('/contacts', function() {
-    return "<h1>All contacts</h1>";
-})->name('contacts.index');
-
-Route::get('/contacts/create', function() {
-    return "<h1>Add new contact</h1>";
-})->name('contacts.create');
-
-// php artisan route:list --except-vendor // To see in console the routes displayed
-// php artisan route:list --path=contacts -r // To show the paths with contacts related
-
-Route::get('/contacts/{id}', function( $id ) {
-    return "Contact: " . $id;
-})->where('id', '[0-9]+')->name('contacts.show'); //->whereNumber('id');
-
-Route::get('/companies/{name?}', function( $name = null ) {
-    if ( $name ){
-        return "Company: " . $name;
-    } else{
-        return "All companies";
-    }
-})->where('name', '[a-zA-Z]+'); //->whereAlpha('name'); || ->whereAlphaNumeric('name');
+Route::prefix('admin')->group(function() {
+    Route::get('/contacts', function() {
+        return "<h1>All contacts</h1>";
+    })->name('contacts.index');
+    
+    Route::get('/contacts/create', function() {
+        return "<h1>Add new contact</h1>";
+    })->name('contacts.create');
+    
+    // php artisan route:list --except-vendor // To see in console the routes displayed
+    // php artisan route:list --path=contacts -r // To show the paths with contacts related
+    
+    Route::get('/contacts/{id}', function( $id ) {
+        return "Contact: " . $id;
+    })->where('id', '[0-9]+')->name('contacts.show'); //->whereNumber('id');
+    
+    Route::get('/companies/{name?}', function( $name = null ) {
+        if ( $name ){
+            return "Company: " . $name;
+        } else{
+            return "All companies";
+        }
+    })->where('name', '[a-zA-Z]+'); //->whereAlpha('name'); || ->whereAlphaNumeric('name');
+});
