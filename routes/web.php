b858c12f211162bebbcda1ef4f2e90de86035890
Josue Martinez <jmartinezolivares1@gmail.com>
2022-10-21 07:14:11 -0600

    Named routes

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $html = "
+        <h1>Contact App</h1>
+        <div>
+            <a href='" . route('contacts.index') . "'>All contacts</a>
+            <a href='" . route('contacts.create') . "'>Add contact</a>
+            <a href='" . route('contacts.show', 1) . "'>Show contact</a>
+        </div>
+    ";
+    return $html;
+})->name('');
 
 Route::get('/contacts', function() {
     return "<h1>All contacts</h1>";
-});
+})->name('contacts.index');
 
 Route::get('/contacts/create', function() {
     return "<h1>Add new contact</h1>";
-});
+})->name('contacts.create');
 
 // php artisan route:list --except-vendor // To see in console the routes displayed
 // php artisan route:list --path=contacts -r // To show the paths with contacts related
 
 Route::get('/contacts/{id}', function( $id ) {
     return "Contact: " . $id;
-})->where('id', '[0-9]+'); //->whereNumber('id');
+})->where('id', '[0-9]+')->name('contacts.show'); //->whereNumber('id');
 
 Route::get('/companies/{name?}', function( $name = null ) {
     if ( $name ){
