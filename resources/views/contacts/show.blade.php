0016633408df0fe2a4992f166f302af0d7e0f924
Josue Martinez <jmartinezolivares1@gmail.com>
2022-10-22 11:13:33 -0600

    Another approach passing data to the views

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -4,14 +4,16 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>{{ "Contact " . 1 }}</title>
+        <title>Details</title>
     </head>
     <body>
-        <h1>{{ "Contact: " . 1 }}</h1>
+        <h1>Contact details</h1>
         <div>
-            <a href="{{ route('contacts.index') }}">All contacts</a>
-            <a href="{{ route('contacts.create') }}">Add contact</a>
-            <a href="../../">Home</a>
+            Name: {{ $contact['name'] }}
+            Phone: {{ $contact['phone'] }}
+        </div>
+        <div>
+            <a href="{{ route('contacts.index') }}">Back to all contacts</a>
         </div>
     </body>
 </html>
