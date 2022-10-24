f33a049c5a56909f0443f39e79d40d144d31bad9
Josue Martinez <jmartinezolivares1@gmail.com>
2022-10-24 08:01:48 -0600

    Own style

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -4,6 +4,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../../../css/style.css">
+        <link rel="shortcut icon" href="../../img/add-contact.png" type="image/x-icon">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
         <title>Create</title>
     </head>
     <body>
