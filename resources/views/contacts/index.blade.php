bb26266103e9ff91d0b365f3ecb06dce032c06c4
Josue Martinez <jmartinezolivares1@gmail.com>
2022-10-24 16:00:06 -0600

    Including sub-views

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -20,29 +20,8 @@
                 <div class="row">
                     <div class="col-md-6"></div>
                     <div class="col-md-6">
-                    <div class="row">
-                        <div class="col">
-                        <select class="custom-select">
-                            <option value="" selected>All Companies</option>
-                            <option value="1">Company One</option>
-                            <option value="2">Company Two</option>
-                            <option value="3">Company Three</option>
-                        </select>
-                        </div>
-                        <div class="col">
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Search..." aria-label="Search..." aria-describedby="button-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-outline-secondary" type="button">
-                                    <i class="fa fa-refresh"></i>
-                                </button>
-                            <button class="btn btn-outline-secondary" type="button" id="button-addon2">
-                                <i class="fa fa-search"></i>
-                            </button>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
+                        @includeif('contacts._filter') 
+                        {{-- Avoids the error displaying the view only if exists --}}
                     </div>
                 </div>
                 <table class="table table-striped table-hover">
