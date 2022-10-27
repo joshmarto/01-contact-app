cefd0c5bd31a3552206e9fc696d55a5de53207c9
Josue Martinez <jmartinezolivares1@gmail.com>
2022-10-27 09:46:42 -0600

    Creating our first controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,14 +26,7 @@ Route::get('/', function () {
 })->name('');
 
 Route::prefix('admin')->group(function() {
-    Route::get('/contacts', function() {
-        $companies = [
-            1 => ['name' => 'Company One', 'contacts' => 3],
-            2 => ['name' => 'Company Two', 'contacts' => 5],
-        ];
-        $contacts = getContacts();
-        return view( 'contacts.index', compact('contacts', 'companies') );
-    })->name('contacts.index');
+    Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
     
     Route::get('/contacts/create', function() {
         return view('contacts.create');
