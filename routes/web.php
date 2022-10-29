148f2eb0a33e813cb4e8ef46be1c9367a94aa36a
Josue Martinez <jmartinezolivares1@gmail.com>
2022-10-29 14:15:12 -0600

    Grouping the controller routes

--- routes/web.php
+++ routes/web.php
@@ -16,27 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('');
-
-Route::prefix('admin')->group(function() {
-    Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
-    
-    Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-    
-    Route::get('/contacts/{id}', [ContactController::class, 'show'])->where('id', '[0-9]+')->name('contacts.show'); //->whereNumber('id');
-    
-    // php artisan route:list --except-vendor // To see in console the routes displayed
-    // php artisan route:list --path=contacts -r // To show the paths with contacts related
-    
-    Route::get('/companies/{name?}', function( $name = null ) {
-        if ( $name ){
-            return "Company: " . $name;
-        } else{
-            return "All companies";
-        }
-    })->where('name', '[a-zA-Z]+'); //->whereAlpha('name'); || ->whereAlphaNumeric('name');
 });
 
-Route::fallback(function() {
-    return "<h1>Sorry, the page does not exist :(</h1>";
+Route::controller(ContactController::class)->name('contacts.')->group(function() {
+    Route::get('/contacts', 'index')->name('index');
+    Route::get('/contacts/create', 'create')->name('create');
+    Route::get('/contacts/{id}', 'show')->where('id', '[0-9]+')->name('show');
 });
+
