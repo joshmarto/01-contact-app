7ca39200b47ff3e91f1044ee9f6a559a631303de
Josue Martinez <jmartinezolivares1@gmail.com>
2022-12-05 14:20:47 -0600

    Dropping foreign keys

--- database/migrations/2022_12_05_153135_add_user_id_to_tasks.php
+++ database/migrations/2022_12_05_153135_add_user_id_to_tasks.php
@@ -27,6 +27,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeign('tasks_user_id_foreign');
             $table->dropColumn('user_id');
         });
     }
