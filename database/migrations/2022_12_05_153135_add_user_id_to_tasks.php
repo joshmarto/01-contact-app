b1f3b04aa344b5c16d67473b3370bf26dc2e4596
Josue Martinez <jmartinezolivares1@gmail.com>
2022-12-05 19:19:20 -0600

    Foreign key constrains actions

--- database/migrations/2022_12_05_153135_add_user_id_to_tasks.php
+++ database/migrations/2022_12_05_153135_add_user_id_to_tasks.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id')->after('id');
-            $table->foreign('user_id')->references('id')->on('users');
+            // $table->unsignedBigInteger('user_id')->after('id')->nullable();
+            $table->foreignId('user_id')->after('id')->nullable()->constrained()->cascadeOnUpdate()->cascadeOnDelete();
         });
     }
 
